76cb1dbfeff6b2b4334604dd4c2d08171cd6e5aa
= <=>
2020-12-03 11:55:35 -0300

    added dashboard channel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var bodyParser = require('body-parser');
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 9001;
 
 app.use(bodyParser.urlencoded({
   extended: true
@@ -18,18 +18,17 @@ app.use(function(req, res, next) {
 io.on('connect', function(socket){
   console.log('connected');
   socket.on('broadcast', function(msg){
-    console.log(msg);
     io.emit('broadcast', msg.toString());
   });
+  socket.on('dashboard'), function (signal){
+    io.emit('dashboard', signal)
+  }
 });
 app.post('/broadcast', function(request, response){
   let message = request.body.message
     io.emit('broadcast', message.toString());
   response.json({message: message, status: 'sent'});
 });
-
-
-
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
